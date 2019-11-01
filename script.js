8e6eb8bac262f086aecd8661d44254d173458846
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-11-01 23:37:10 +0100

    Created some of the two first screens

--- script.js
+++ script.js
@@ -2,28 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", fetchFiles);
 const text_content = document.querySelector(".text_content");
+const video_content = document.querySelector(".video_content");
 
 let index = 0;
+let animationOngoing = false;
 
-// function fetchSVGs() {
-//     fetch('elements/test.svg')
-//         .then (r => r.text())
-//         .then (text => {
-//             document.querySelector(".video_container").innerHTML = text;
-//         });
-// }
+let camillaVideo;
 
 function fetchFiles() {
 
-    const circle = fetch("elements/test.svg").then(r => r.text());
+    const video1 = fetch("elements/video1.svg").then(r => r.text());
     const circle2 = fetch("elements/test.svg").then(r => r.text());
 
     Promise
-        .all([circle, circle2])
+        .all([video1])
         .then(
             function(responses) {
-                const [circle, circle2] = responses;
+                const [video1] = responses;
                 // document.querySelector(".video_container").innerHTML = circle;
+                camillaVideo = video1;
                 // document.querySelector(".video_container").innerHTML += circle2;
                 run1stScreen();
             }
@@ -34,17 +31,29 @@ function run1stScreen() {
     let textContent = "Assignment: 14C.01.05 GTA intro";
     text_content.classList.add("intro_text");
 
-    typewriterEffect(textContent);
+    typewriterEffect(textContent, run2ndScreen());
+    if (!animationOngoing) {
+        console.log("done");
+    }
 }
 
-function typewriterEffect(text) {
-    if (index <= text.length) {
-        console.log(text);
-        text_content.textContent = text.substring(0, index);
-        index++;
-        setTimeout(function() {
-    typewriterEffect(text);
-            }
-    , 200);
-    }
+function typewriterEffect(text, nextFunction) {
+    // animationOngoing = true;
+    // if (index <= text.length) {
+    //     console.log(index);
+    //     text_content.textContent = text.substring(0, index);
+    //     index++;
+    //     setTimeout(function() {
+    // typewriterEffect(text);
+    //         }
+    // , 200);
+    // }
+    // else {
+        nextFunction;
+    // }
+}
+
+function run2ndScreen() {
+    text_content.style.display = "none";
+    video_content.innerHTML = camillaVideo;
 }
